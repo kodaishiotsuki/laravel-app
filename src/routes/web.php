3a3f385d13114f9c33d815f1f548ee1ab2039c3a
kodaishiotsuki <0711dr5@gmail.com>
2022-06-12 12:22:53 +0900

    ルーティング

--- src/routes/web.php
+++ src/routes/web.php
@@ -24,6 +24,6 @@ Route::get('/home', function () {
 
 Route::get('/about', function () {
     return view('about');
-})->middleware('check');
+});
 
-Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/contacted', [ContactController::class, 'index'])->name('con');
