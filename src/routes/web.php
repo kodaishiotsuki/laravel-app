d4e2204fe6028832c01cbb050c91ef4d9cb500e5
kodaishiotsuki <0711dr5@gmail.com>
2022-06-12 13:52:32 +0900

    認証の詳細

--- src/routes/web.php
+++ src/routes/web.php
@@ -28,12 +28,16 @@ Route::get('/about', function () {
 
 Route::get('/contacted', [ContactController::class, 'index'])->name('con');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
